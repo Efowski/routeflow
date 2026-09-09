eb97f8f7a6fa0c4e97aa2d0a141d43c421c5d590
Pawel Mazur <efowski@gmail.com>
2026-09-09 22:52:21 +0200

    Fix gym creation during manager registration

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -2,39 +2,57 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from .models import Gym
+
+
 User = get_user_model()
+
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'role', 'gym', 'gym_name', 'password']
+        fields = [
+            'id',
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'role',
+            'gym',
+            'gym_name',
+            'password',
+        ]
 
     def create(self, validated_data):
+        gym_name = validated_data.get('gym_name', 'VertiGym Warszawska')
+        gym = validated_data.get('gym')
+
+        if not gym:
+            gym = Gym.objects.create(name=gym_name)
+
         user = User.objects.create_user(
             email=validated_data['email'],
             username=validated_data.get('username') or validated_data['email'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
             role=validated_data.get('role', 'gym_manager'),
-            gym=validated_data.get('gym'),
-            gym_name=validated_data.get('gym_name', 'VertiGym'),
-            password=validated_data['password']
+            gym=gym,
+            gym_name=gym_name,
+            password=validated_data['password'],
         )
+
         return user
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
-
     name = serializers.SerializerMethodField()
     gym_name = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-
         fields = ['id', 'email', 'name', 'role', 'gym', 'gym_name']
-
         read_only_fields = [
             'id',
             'email',
@@ -60,18 +78,23 @@ class GymUserSeralizer(serializers.ModelSerializer):
 
     def get_name(self, obj):
         return obj.get_full_name() or obj.username
-        
-    
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
+
         data['user'] = {
             'id': str(self.user.id),
             'email': self.user.email,
             'name': self.user.get_full_name(),
             'role': self.user.role,
             'gym': str(self.user.gym.id) if self.user.gym else None,
-            'gym_name': self.user.gym_name,
+            'gym_name': (
+                self.user.gym.name
+                if self.user.gym
+                else self.user.gym_name
+            ),
         }
+
         return data
